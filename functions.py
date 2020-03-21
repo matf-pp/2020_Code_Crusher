1e5e5ba26ee5ddcc0cc75b932a9c118b8160a476
Igor Scekic <igorscekic98@gmail.com>
2020-03-21 23:36:47 +0100

    Implementirane funkcije: swap, vLineAt, hLineAt

--- functions.py
+++ functions.py
@@ -5,4 +5,22 @@ def swap(board, r1, c1, r2, c2):
 
 def vLineAt(board, r, c):
     location = board[r][c]
-    if r 
+    if r-2 >= 0 and location == board[r-1][c] and location == board[r-2][c]:
+        return True
+    if r-1 >= 0 and r+1<len(board) and location == board[r-1][c] and location == board[r+1][c]:
+        return True
+    if r+2<len(board) and location == board[r+1][c] and location == board[r+2][c]:
+        return True
+    
+    return False
+
+def hLineAt(board, r, c):
+    location = board[r][c]
+    if c-2 >= 0 and location == board[r][c-1] and location == board[r][c-2]:
+        return True
+    if c-1 >= 0 and c+1<len(board[r]) and location == board[r][c-1] and location == board[r][c+1]:
+        return True
+    if c+2<len(board[r]) and location == board[r][c+1] and location == board[r][c+2]:
+        return True
+    
+    return False
